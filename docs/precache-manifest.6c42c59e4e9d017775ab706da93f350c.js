f718d0c0b85b12c6f95995e99aa31e684fd20093
LarsDekker <lars_dekker@live.nl>
2022-04-06 12:58:15 +0200

    Adds docs build

---
+++ docs/precache-manifest.6c42c59e4e9d017775ab706da93f350c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2f95ca789a0bb189d001af9ed95ea6e8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f8e37eb443736409f97c",
+    "url": "/static/css/main.b41928ae.chunk.css"
+  },
+  {
+    "revision": "4d7543b1379cd51bc6de",
+    "url": "/static/js/2.f7d09eee.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/static/js/2.f7d09eee.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f8e37eb443736409f97c",
+    "url": "/static/js/main.a77a521c.chunk.js"
+  },
+  {
+    "revision": "1d7cc1b1de4480421ba4",
+    "url": "/static/js/runtime-main.b571ac3b.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
